9a3c2e406dcfaf5f516754e6b90863e6d5893a2e
corey <coreyg@vimeo.com>
2014-11-18 17:10:16 -0500

    Update core more

--- config/cjg-core.el
+++ config/cjg-core.el
@@ -10,6 +10,8 @@
 
 (setq make-backup-files nil)
 
+(set-fringe-style '(8 . 0))
+
 ;; from <https://github.com/bling/dotemacs/>
 (defmacro after (feature &rest body)
   "After FEATURE is loaded, evaluate BODY."
@@ -17,12 +19,28 @@
   `(eval-after-load ,feature
           '(progn ,@body)))
 
+
+;; Hide startup messages
+(setq inhibit-splash-screen t
+      inhibit-startup-echo-area-message t
+      inhibit-startup-message t)
+
 ;; make sure $PATH is set correctly
 (use-package exec-path-from-shell
     :ensure exec-path-from-shell)
 
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 (menu-bar-mode -1)
+
+;; Let me write `y` or `n` even for important stuff that would normally require
+;; me to fully type `yes` or `no`.
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Flash the frame to represent a bell.
+(setq visible-bell t)
+;; nevermind that's annoying
+(setq ring-bell-function 'ignore)
 
 (provide 'cjg-core)
