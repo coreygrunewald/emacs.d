1db7f79e954e5b273adbb88bfaa951b228d606a5
Corey Grunewald <cgrunewald@netflix.com>
2015-01-10 22:11:56 -0800

    Add capture templates

--- config/languages/cjg-org.el
+++ config/languages/cjg-org.el
@@ -10,8 +10,15 @@
   :config
   (progn
     (setq org-log-done t)
-    (setq org-default-notes-file "~/.notes.org")
-    ))
+    (setq org-default-notes-file (expand-file-name "~/org/notes.org"))
+    (setq org-capture-templates
+          (quote (("t" "todo" entry (file (expand-file-name "~/org/todo.org"))
+                   "* TODO %?\n %i\n %a")
+                  ;; TODO: figure out org mode and capture templates!
+                  ("n" "note" entry (file (expand-file-name "~/org/notes.org"))
+                   "* %? :NOTE:\n %U\n %a\n" :prepend t)
+                  )))
 
+    ))
 
 (provide 'cjg-org)
