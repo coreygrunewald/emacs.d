6bffca6f50d7fa4dc6d9af408bc9f3dfbd1ad633
Corey Grunewald <cgrunewald@netflix.com>
2015-01-10 22:12:30 -0800

    Change groovy indentation

--- config/languages/cjg-groovy.el
+++ config/languages/cjg-groovy.el
@@ -2,8 +2,10 @@
   :mode "\\.groovy?\\'"
   :config (progn
     ;;; make Groovy mode electric by default.
+            (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
             (add-hook 'groovy-mode-hook
                       '(lambda ()
+                         (setq c-basic-offset 4)
                          (require 'groovy-electric)
                          (groovy-electric-mode)))))
 
