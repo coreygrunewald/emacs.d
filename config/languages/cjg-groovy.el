d876a5dd95597ef3749cd905307326d92ecdee41
Corey Grunewald <cjgrunewald@gmail.com>
2015-01-22 23:15:38 -0800

    Fix helm dash docsets

--- config/languages/cjg-groovy.el
+++ config/languages/cjg-groovy.el
@@ -1,8 +1,14 @@
+(defun cjg/groovy-doc ()
+  (interactive)
+  (after 'helm-dash
+    (setq-local helm-dash-docsets '("Groovy"))))
+
 (use-package groovy-mode
   :mode "\\.groovy?\\'"
   :config (progn
     ;;; make Groovy mode electric by default.
             (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+            (add-hook 'groovy-mode-hook 'cjg/groovy-doc)
             (add-hook 'groovy-mode-hook
                       '(lambda ()
                          (setq c-basic-offset 4)
--- config/languages/cjg-js.el
+++ config/languages/cjg-js.el
@@ -1,3 +1,8 @@
+(defun cjg/js-doc ()
+  (interactive)
+  (after 'helm-dash
+    (setq-local helm-dash-docsets '("JavaScript" "NodeJS"))))
+
 (use-package js2-mode
   :ensure js2-mode
   :mode "\\.js?\\'"
@@ -6,10 +11,10 @@
     (add-to-list 'interpreter-mode-alist '("node" . js2-mode)))
   :config
   (progn
+    (add-hook 'js2-mode-hook 'cjg/js-doc)
     (add-hook 'js2-mode-hook
-              (lambda ()
-                (js2-imenu-extras-setup)
-                (setq-local helm-dash-docsets '("JavaScript"))))
+              '(lambda ()
+                (js2-imenu-extras-setup)))
 
     (after 'js2-mode
       (setq js2-highlight-level 3)
